be533ae9a696762ffdf8328ed4315aabe503b402
Yash890-star <yashwanthk523@gmail.com>
2024-11-04 10:41:32 +0530

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./static/**/*.{css,html,js}",
+    "./templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
